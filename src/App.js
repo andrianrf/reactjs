f8ee588b6c8b2eff28f8bf2b6bc40034f4e3dc06
LAPTOP-AH657H2F\62896 <andrian.febriana@multipolar.com>
2021-09-05 23:37:47 +0700

    finish react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2> Halaman Home </h2>
+}
+
+function Users() {
+  return (
+  <React.Fragment>
+  <h2> Halaman Users </h2>
+  <ul>
+    <li> <Link to='/user/andrian'>Andrian</Link> </li>
+    <li> <Link to='/user/ramadhan'>Ramadhan</Link> </li>
+    <li> <Link to='/user/febriana'>Febriana</Link> </li>
+  </ul>
+  </React.Fragment>
+  )  
+}
+
+function User({match}) {
+
+  return <h2> Ini halaman {match.params.name} </h2>
+  
+}
+
+function NotMatch() {
+  return <h2> Halaman Tidak Ditemukan </h2>  
+}
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-    }
-  }
-
   render(){
     return (
-      <div>
-      </div>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <li> <Link to='/'> Home </Link> </li>
+            <li> <Link to='/users'> Users </Link> </li>
+          </nav>
+
+          <main>
+            <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/users' exact component={Users} />
+            <Route path='/user/:name' exact component={User} />
+            <Route component={NotMatch} />
+            </Switch>
+          </main>
+        </div>      
+      </BrowserRouter>
     )
   }
 }
