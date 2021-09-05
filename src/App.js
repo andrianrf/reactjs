709c6477f5dfc84d9715a4a5644375236e082d6a
LAPTOP-AH657H2F\62896 <andrian.febriana@multipolar.com>
2021-09-05 23:16:41 +0700

    finish konsumsi-api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import { Component } from 'react';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(Response => Response.json())
+    .then(data => this.setState({items:data, isLoading:false}))
+  }
 
   render(){
+
+    const { items, isLoading } = this.state;
+
+    if(isLoading){
+      return <p>Loading...</p>
+    }
+
     return (
       <div>
+        <ul>
+          { items.map((item, index) =>
+          <li key={index}> {item.name} </li>
+          ) }
+        </ul>
       </div>
     )
   }
