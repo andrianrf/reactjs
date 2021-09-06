adcb0923493dea41fbcc5225da72387650700009
LAPTOP-AH657H2F\62896 <andrian.febriana@multipolar.com>
2021-09-06 21:54:30 +0700

    initialize api-fake

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import { Component, Fragment } from 'react';
 import ApiGet from './class/ApiGet';
-import ApiGetV2 from './class/ApiGetV2';
-import ApiGetV3 from './class/ApiGetV3';
-
 
 class App extends Component {
 
@@ -11,12 +8,8 @@ class App extends Component {
 
     return (
       <Fragment>
-        <h3>ApiGet V1</h3>
+        <h3>ApiGet</h3>
         <ApiGet />
-        <h3>ApiGet V2</h3>
-        <ApiGetV2 />
-        <h3>ApiGet V3</h3>
-        <ApiGetV3 />
       </Fragment>
     )
   }
