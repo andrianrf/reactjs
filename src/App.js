bd411d2371fbba17361aeb38fbd4b9a835585835
LAPTOP-AH657H2F\62896 <andrian.febriana@multipolar.com>
2021-09-07 23:30:44 +0700

    remove exact from users

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Users from './pages/Users';
+import User from './pages/User';
 
 function NotMatch() {
   return <h2> Halaman Tidak Ditemukan </h2>  
@@ -16,13 +17,14 @@ class App extends Component {
         <div>
           <nav>
             <li> <Link to='/'> Home </Link> </li>
-            <li> <Link to='/users'> Users </Link> </li>
+            <li> <Link to='/user'> Users </Link> </li>
           </nav>
 
           <main>
             <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/users' exact component={Users} />
+            <Route path='/user' component={Users} />
+            <Route path='/user/:name' exact component={User} />
             <Route component={NotMatch} />
             </Switch>
           </main>
