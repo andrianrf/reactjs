ab6f9d636fd382c307845691b8e0f1e88f088654
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-09-29 17:43:18 +0700

    hooks lifecycle

--- src/hook/hook.jsx
+++ src/hook/hook.jsx
@@ -1,9 +1,21 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Component } from "react";
 
 // class Hook extends Component {
 //     state = {
 //         counter: 0
+//     }
+
+//     componentDidMount(){
+//         document.title = `Hook ${this.state.counter}`;
+//     }
+
+//     componentDidUpdate(){
+//         document.title = `Hook ${this.state.counter}`;
+//     }
+
+//     componentWillUnmount(){
+//         document.title = `ReactJS`;
 //     }
 
 //     plus = () => {
@@ -24,6 +36,14 @@ import { Component } from "react";
 
 const Hook = () => {
     const [counter, plus] = useState(0);
+
+    useEffect(() => {
+        document.title = `Hook : ${counter}`;
+        return () => {
+            document.title = `ReactJS`;
+        }
+    });
+
     return(
         <Fragment>
             Counter : {counter}
