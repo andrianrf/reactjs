ebafd41beb1310bf05519259d50a6430ae717bb6
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-01 22:42:42 +0700

    add loading while register

--- src/pages/firebase-auth/Register.js
+++ src/pages/firebase-auth/Register.js
@@ -23,8 +23,10 @@ class Register extends Component {
 
     handleSubmit = () => {
         const {email, password} = this.state;
-
+    
         const auth = getAuth(firebaseApp);
+    
+        this.props.reduxChangeIsLoading(true);
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
@@ -32,6 +34,7 @@ class Register extends Component {
                     // ...
                     console.log("user: ", user);
                     console.log("success: ", userCredential);
+                    this.props.reduxChangeIsLoading(false);
         })
         .catch((error) => {
             var errorCode = error.code;
@@ -39,8 +42,10 @@ class Register extends Component {
             // ..
             console.log("error code: ", errorCode);
             console.log("error message: ", errorMessage);
+            this.props.reduxChangeIsLoading(false);
         });
     }
+
 
     render(){
         return (
@@ -50,7 +55,8 @@ class Register extends Component {
                         <p className="auth-title">Register {this.props.reduxUsername} </p>
                         <input className="input" id="email" type="text" onChange={this.handleChangeText} placeholder="Email" />
                         <input className="input" id="password" type="password" onChange={this.handleChangeText} placeholder="Password" />
-                        <Button onClick={this.handleSubmit} value="Register" loading={this.props.reduxIsLoading } />
+
+                        <Button onClick={() => this.handleSubmit()} value="Register" loading={this.props.reduxIsLoading } />
                         <Button onClick={() => this.props.reduxLoading({message:"message loading"})} value="Loading" loading={this.props.reduxIsLoading } />
                         <Button onClick={this.props.reduxChangeUsername} value="Change Username" loading={this.props.reduxIsLoading } />
                     </div>
@@ -68,7 +74,8 @@ const reduxState = (state) =>({
 
 const reduxReducer = (dispatch) => ({
     reduxChangeUsername: () => dispatch({type: "CHANGE_USERNAME", value: "Andrian Ramadhan Febriana"}),
-    reduxLoading: (data) => dispatch(Loading(data))
+    reduxLoading: (data) => dispatch(Loading(data)),
+    reduxChangeIsLoading: (data) => dispatch({type: "CHANGE_LOADING", value: data})
 })
 
 export default connect(reduxState, reduxReducer)(Register);
