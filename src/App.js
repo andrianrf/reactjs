bcbb7dedf0d25dae7107cc555ab66f6e55357739
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-04 18:40:56 +0700

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
 import './App.css';
-import { Component, Fragment } from 'react';
+import React from 'react';
 
-class App extends Component {
+export default function App() {
+  return (
+    <div>
+      <ol>
+        <li>
+          ES7 React/Redux/GraphQL (untuk autocreate komponen react) : Shift+P &gt; ES7 &gt; function / class &gt; rcc
+        </li>
+        <li>
+          ESLint (untuk menstandarisasikan code)
+          <br />
+          npm install -g eslint
+          <br />
+          eslint --init (check syntax, find problems, and enforcecode style; Javascript Modules; React; No; Browser; Use a popular style; Airbnb; JSON; Y;) .eslintrc.json
+          <br />
+          install ESLint extension for vscode
+          <br />
+          eslint src/ (untuk mengecek sourcecode)
+          <br />
+          eslint src/ --fix (untuk auto membetulkan code)
+          <br />
+          settings.json ("editor.codeActionsOnSave": &#123; "source.fixAll.eslint": true &#125;)
+          .eslintrc.json ("rules": &#123; "no-console": "off" &#125;) (untuk exclude rule)
 
-  render(){
-    return (
-      <Fragment>
-        <ol>
-          <li>ES7 React/Redux/GraphQL : Shift+P > ES7 > function / class</li>
-          <li></li>
-        </ol>
-      </Fragment>
-    )
-  }
+        </li>
+      </ol>
+    </div>
+  );
 }
-
-export default App;
