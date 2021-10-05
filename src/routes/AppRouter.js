2e994bf220552902e19304f503695f0e17ddd386
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-05 18:10:17 +0700

    approuter

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Redirect, Route, Switch } from 'react-router-dom'
 
 import { useKeycloak } from '@react-keycloak/web'
 
@@ -16,9 +16,16 @@ export const AppRouter = () => {
 
   return (
     <Router>
+    <nav>
+      <li> <Link to='/home'> Home </Link> </li>
+      <li> <Link to='/login'> Login </Link> </li>
+      <li> <Link to='/users'> Users </Link> </li>
+    </nav>
+    <Switch>
       <Redirect from="/" to="/home" />
       <PrivateRoute path="/home" component={HomePage} />
       <Route path="/login" component={LoginPage} />
+    </Switch>
     </Router>
   )
 }
