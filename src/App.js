fae87de1a3ddc14f96e0b50611d93b986398297c
Andrian Ramadhan Febriana <andrian.febriana@multipolar.com>
2021-10-05 14:36:48 +0700

    finish integrate with keycloak

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import Secured from './pages/Secured';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-    }
-  }
-
   render(){
     return (
-      <div>
-        React App.js
-      </div>
-    )
+      <BrowserRouter>
+        <div className="container">
+          npm install @react-keycloak/web keycloak-js --save
+          <ul>
+            <li>
+              <Link to="/">public component</Link>
+            </li>
+            <li>
+              <Link to="/secured">secured component</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
